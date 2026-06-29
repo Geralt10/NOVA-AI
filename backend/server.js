23ba852e4948a935ffbe899972ec336ee0d5534a
himanshu dhoundiyal <105155811+Geralt10@users.noreply.github.com>
2026-06-29 23:26:43 +0530

    mail service change

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ async function startServer() {
     console.error(error);
   }
 }
+console.log("Node Version:", process.version);
 startServer();
 httpServer.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
